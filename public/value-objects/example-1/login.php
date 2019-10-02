dc298131b84761691bff1728bcf19b2c8e2c93c2
Matej Bádal <matejbadal@gmail.com>
2019-10-02 21:10:10 +0200

    fixed bugs

--- public/value-objects/example-1/login.php
+++ public/value-objects/example-1/login.php
@@ -1,21 +1,29 @@
-<?php
+<?php declare(strict_types=1);
+
+require '../../../vendor/autoload.php';
+
 
 $email    = $_POST['email'];
 $password = $_POST['password'];
 
-$usersJson  = file_get_contents('users.json');
+$usersJson  = file_get_contents('../../../users.json');
 $usersArray = json_decode($usersJson, true);
 
 $users = array_filter($usersArray, function ($item) use ($email) {
     return ($item['email'] === $email);
 });
 
-if (sizeof($users) !== 1) {
-    echo 'Redirect to Login';
+if (sizeof($users) > 1) {
+    echo 'Redirect to Login. Multiple users with email found.';
     exit;
 }
 
-$user = $users[0];
+if (sizeof($users) === 0) {
+    echo "User with email: [{$email}] is not registered";
+    exit;
+}
+
+$user = current($users);
 if (!password_verify($password, $user['password'])) {
     echo 'Invalid Password';
     exit;
--- public/value-objects/example-1/registration.php
+++ public/value-objects/example-1/registration.php
@@ -1,15 +1,27 @@
-<?php
+<?php declare(strict_types=1);
+
+require '../../../vendor/autoload.php';
 
 $email          = $_POST['email'];
 $password       = $_POST['password'];
 $passwordRepeat = $_POST['passwordRepeat'];
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "String: [{$email}] is not a valid email address";
+    exit;
+}
 
 if ($password !== $passwordRepeat) {
     echo 'Password do not match';
     exit;
 }
 
-$usersJson  = file_get_contents('users.json');
+if (strlen($password) < 4) {
+    echo 'Password should be at least 4 characters long';
+    exit;
+}
+
+$usersJson  = file_get_contents('../../../users.json');
 $usersArray = json_decode($usersJson, true);
 
 $users = array_filter($usersArray, function ($item) use ($email) {
@@ -20,3 +32,11 @@ if (!empty($users)) {
     echo 'User with email already exists';
     exit;
 }
+
+$usersArray[] = [
+    'id'       => count($usersArray) + 1,
+    'email'    => $email,
+    'password' => password_hash($password, PASSWORD_DEFAULT)
+];
+file_put_contents('../../../users.json', json_encode($usersArray));
+echo "User with email: [{$email}] was registered successfully";
