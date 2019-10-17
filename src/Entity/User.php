874aa5173af91940d57045787bcde33a3b76a3c2
Matej Bádal <badal@profesia.sk>
2019-10-17 11:34:06 +0200

    created User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace LearningDdd\Entity;
+
+use LearningDdd\ValueObject\EmailAddress;
+use LearningDdd\ValueObject\PasswordHash;
+use LearningDdd\ValueObject\UserId;
+
+class User
+{
+    /** @var UserId */
+    private $id;
+
+    /** @var EmailAddress */
+    private $email;
+
+    /** @var PasswordHash */
+    private $password;
+
+    public function __construct(UserId $id, EmailAddress $email, PasswordHash $password)
+    {
+        $this->id       = $id;
+        $this->email    = $email;
+        $this->password = $password;
+    }
+
+    public function getId(): UserId
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): EmailAddress
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): PasswordHash
+    {
+        return $this->password;
+    }
+}
