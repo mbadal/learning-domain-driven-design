fde8867a453f265a56e446f4793076418ec130af
Matej Bádal <badal@profesia.sk>
2020-01-24 17:57:42 +0100

    added missing validation

--- public/value-objects/example-1/login.php
+++ public/value-objects/example-1/login.php
@@ -3,6 +3,17 @@
 $email    = $_POST['email'];
 $password = $_POST['password'];
 
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo 'Supplied email address is not valid';
+    exit;
+}
+
+$passwordLength = strlen($password);
+if (0 === $passwordLength || $passwordLength < 6) {
+    echo 'Password should be at least 6 characters long';
+    exit;
+}
+
 $usersJson  = file_get_contents('users.json');
 $usersArray = json_decode($usersJson, true);
 
@@ -10,14 +21,19 @@ $users = array_filter($usersArray, function ($item) use ($email) {
     return ($item['email'] === $email);
 });
 
-if (sizeof($users) !== 1) {
-    var_dump('Redirect to Login');
+if (sizeof($users) > 1) {
+    echo('Fatal error. More than one user with email found in DB');
+    exit;
+}
+
+if (sizeof($users) === 0) {
+    echo('Invalid user');
     exit;
 }
 
 $user = $users[0];
 if (!password_verify($password, $user['password'])) {
-    var_dump('Invalid Password');
+    echo('Invalid Password');
     exit;
 }
 
--- public/value-objects/example-1/registration.php
+++ public/value-objects/example-1/registration.php
@@ -4,8 +4,19 @@ $email          = $_POST['email'];
 $password       = $_POST['password'];
 $passwordRepeat = $_POST['passwordRepeat'];
 
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo 'Supplied email address is not valid';
+    exit;
+}
+
+$passwordLength = strlen($password);
+if (0 === $passwordLength || $passwordLength < 6) {
+    echo 'Password should be at least 6 characters long';
+    exit;
+}
+
 if ($password !== $passwordRepeat) {
-    echo 'Password do not match';
+    echo 'Passwords do not match';
     exit;
 }
 
@@ -20,3 +31,11 @@ if (!empty($users)) {
     echo 'User with email already exists';
     exit;
 }
+
+$usersArray[] = [
+    'id'    => sizeof($usersArray),
+    'email' => $email,
+    'password' => password_hash($password, PASSWORD_DEFAULT)
+];
+echo 'Successfully registered';
+exit;
